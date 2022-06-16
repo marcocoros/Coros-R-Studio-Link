6c7b7e3cf062d929b97f0a2f4e6903d01acf10bc
marcocoros <marco.coros@obf.ateneo.edu>
2022-06-16 19:25:24 +0800

    Marco's First Commit

---
+++ Short Intro.R
@@ -0,0 +1,4 @@
+print("Hello World!")
+print("My name is Marco Luis Coros.")
+print("You can call me Marco.")
+print("I am a Computer Engineering Student currently studying in Ateneo de Manila University.")
